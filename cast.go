a440c9d4ff3ee1d12f3e6e158de20f12ecb3cbeb
lizongti <lzongti@gmail.com>
2022-11-13 22:16:55 +0800

    append args for some parser.

--- cast.go
+++ cast.go
@@ -2336,13 +2336,13 @@ func ToErrorE(a any) (error, error) {
 }
 
 // ToTime casts an interface to a time.Time type.
-func ToTime(a any) time.Time {
+func ToTime(a any, args ...any) time.Time {
 	t, _ := ToTimeE(a)
 	return t
 }
 
 // ToTimeE casts an interface to a time.Time type.
-func ToTimeE(a any) (time.Time, error) {
+func ToTimeE(a any, args ...any) (time.Time, error) {
 	a = indirectToStringerOrError(a)
 
 	switch v := a.(type) {
@@ -2392,11 +2392,11 @@ func ToTimeE(a any) (time.Time, error) {
 	case time.Duration:
 		return time.Unix(0, int64(v)), nil
 	case string:
-		return parseTime(v)
+		return parseTime(v, args...)
 	case []byte:
-		return parseTime(string(v))
+		return parseTime(string(v), args...)
 	case fmt.Stringer:
-		return parseTime(v.String())
+		return parseTime(v.String(), args...)
 	case nil:
 		return time.Time{}, nil
 	default:
@@ -2405,13 +2405,13 @@ func ToTimeE(a any) (time.Time, error) {
 }
 
 // ToDuration casts an interface to a time.Duration type.
-func ToDuration(i any) time.Duration {
+func ToDuration(i any, args ...any) time.Duration {
 	d, _ := ToDurationE(i)
 	return d
 }
 
 // ToDurationE casts an interface to a time.Duration type.
-func ToDurationE(a any) (d time.Duration, err error) {
+func ToDurationE(a any, args ...any) (d time.Duration, err error) {
 	a = indirectToStringerOrError(a)
 
 	switch v := a.(type) {
@@ -2461,13 +2461,13 @@ func ToDurationE(a any) (d time.Duration, err error) {
 	case time.Duration:
 		return v, nil
 	case string:
-		return parseDuration(v)
+		return parseDuration(v, args...)
 	case []byte:
-		return parseDuration(string(v))
+		return parseDuration(string(v), args...)
 	case fmt.Stringer:
-		return parseDuration(v.String())
+		return parseDuration(v.String(), args...)
 	case error:
-		return parseDuration(v.Error())
+		return parseDuration(v.Error(), args...)
 	default:
 		return time.Duration(0), fmt.Errorf("unable to cast %#v of type %T to Duration", a, a)
 	}
@@ -2537,92 +2537,116 @@ loop:
 	return strconv.ParseInt(s, 0, 0)
 }
 
-type timeFormatType int
+type TimeFormatType int
 
 const (
-	timeFormatNoTimezone timeFormatType = iota
-	timeFormatNamedTimezone
-	timeFormatNumericTimezone
-	timeFormatNumericAndNamedTimezone
-	timeFormatTimeOnly
+	TimeFormatNoTimezone TimeFormatType = iota
+	TimeFormatNamedTimezone
+	TimeFormatNumericTimezone
+	TimeFormatNumericAndNamedTimezone
+	TimeFormatTimeOnly
 )
 
 func _() {
 	// An "invalid array index" compiler error signifies that the constant values have changed.
 	// Re-run the stringer command to generate them again.
 	var x [1]struct{}
-	_ = x[timeFormatNoTimezone-0]
-	_ = x[timeFormatNamedTimezone-1]
-	_ = x[timeFormatNumericTimezone-2]
-	_ = x[timeFormatNumericAndNamedTimezone-3]
-	_ = x[timeFormatTimeOnly-4]
+	_ = x[TimeFormatNoTimezone-0]
+	_ = x[TimeFormatNamedTimezone-1]
+	_ = x[TimeFormatNumericTimezone-2]
+	_ = x[TimeFormatNumericAndNamedTimezone-3]
+	_ = x[TimeFormatTimeOnly-4]
 }
 
 const _timeFormatType_name = "timeFormatNoTimezonetimeFormatNamedTimezonetimeFormatNumericTimezonetimeFormatNumericAndNamedTimezonetimeFormatTimeOnly"
 
 var _timeFormatType_index = [...]uint8{0, 20, 43, 68, 101, 119}
 
-func (i timeFormatType) String() string {
-	if i < 0 || i >= timeFormatType(len(_timeFormatType_index)-1) {
+func (i TimeFormatType) String() string {
+	if i < 0 || i >= TimeFormatType(len(_timeFormatType_index)-1) {
 		return "timeFormatType(" + strconv.FormatInt(int64(i), 10) + ")"
 	}
 	return _timeFormatType_name[_timeFormatType_index[i]:_timeFormatType_index[i+1]]
 }
 
-type timeFormat struct {
-	format string
-	typ    timeFormatType
+type TimeFormat struct {
+	Format string
+	Type   TimeFormatType
 }
 
-func (f timeFormat) hasTimezone() bool {
+func (f TimeFormat) hasTimezone() bool {
 	// We don't include the formats with only named timezones, see
 	// https://github.com/golang/go/issues/19694#issuecomment-289103522
-	return f.typ >= timeFormatNumericTimezone && f.typ <= timeFormatNumericAndNamedTimezone
+	return f.Type >= TimeFormatNumericTimezone && f.Type <= TimeFormatNumericAndNamedTimezone
 }
 
 var (
-	timeFormats = []timeFormat{
-		{time.RFC3339, timeFormatNumericTimezone},
-		{"2006-01-02T15:04:05", timeFormatNoTimezone}, // iso8601 without timezone
-		{time.RFC1123Z, timeFormatNumericTimezone},
-		{time.RFC1123, timeFormatNamedTimezone},
-		{time.RFC822Z, timeFormatNumericTimezone},
-		{time.RFC822, timeFormatNamedTimezone},
-		{time.RFC850, timeFormatNamedTimezone},
-		{"2006-01-02 15:04:05.999999999 -0700 MST", timeFormatNumericAndNamedTimezone}, // Time.String()
-		{"2006-01-02T15:04:05-0700", timeFormatNumericTimezone},                        // RFC3339 without timezone hh:mm colon
-		{"2006-01-02 15:04:05Z0700", timeFormatNumericTimezone},                        // RFC3339 without T or timezone hh:mm colon
-		{"2006-01-02 15:04:05", timeFormatNoTimezone},
-		{time.ANSIC, timeFormatNoTimezone},
-		{time.UnixDate, timeFormatNamedTimezone},
-		{time.RubyDate, timeFormatNumericTimezone},
-		{"2006-01-02 15:04:05Z07:00", timeFormatNumericTimezone},
-		{"2006-01-02", timeFormatNoTimezone},
-		{"02 Jan 2006", timeFormatNoTimezone},
-		{"2006-01-02 15:04:05 -07:00", timeFormatNumericTimezone},
-		{"2006-01-02 15:04:05 -0700", timeFormatNumericTimezone},
-		{time.Kitchen, timeFormatTimeOnly},
-		{time.Stamp, timeFormatTimeOnly},
-		{time.StampMilli, timeFormatTimeOnly},
-		{time.StampMicro, timeFormatTimeOnly},
-		{time.StampNano, timeFormatTimeOnly},
+	defaultTimeFormats = []TimeFormat{
+		{time.RFC3339, TimeFormatNumericTimezone},
+		{"2006-01-02T15:04:05", TimeFormatNoTimezone}, // iso8601 without timezone
+		{time.RFC1123Z, TimeFormatNumericTimezone},
+		{time.RFC1123, TimeFormatNamedTimezone},
+		{time.RFC822Z, TimeFormatNumericTimezone},
+		{time.RFC822, TimeFormatNamedTimezone},
+		{time.RFC850, TimeFormatNamedTimezone},
+		{"2006-01-02 15:04:05.999999999 -0700 MST", TimeFormatNumericAndNamedTimezone}, // Time.String()
+		{"2006-01-02T15:04:05-0700", TimeFormatNumericTimezone},                        // RFC3339 without timezone hh:mm colon
+		{"2006-01-02 15:04:05Z0700", TimeFormatNumericTimezone},                        // RFC3339 without T or timezone hh:mm colon
+		{"2006-01-02 15:04:05", TimeFormatNoTimezone},
+		{time.ANSIC, TimeFormatNoTimezone},
+		{time.UnixDate, TimeFormatNamedTimezone},
+		{time.RubyDate, TimeFormatNumericTimezone},
+		{"2006-01-02 15:04:05Z07:00", TimeFormatNumericTimezone},
+		{"2006-01-02", TimeFormatNoTimezone},
+		{"02 Jan 2006", TimeFormatNoTimezone},
+		{"2006-01-02 15:04:05 -07:00", TimeFormatNumericTimezone},
+		{"2006-01-02 15:04:05 -0700", TimeFormatNumericTimezone},
+		{time.Kitchen, TimeFormatTimeOnly},
+		{time.Stamp, TimeFormatTimeOnly},
+		{time.StampMilli, TimeFormatTimeOnly},
+		{time.StampMicro, TimeFormatTimeOnly},
+		{time.StampNano, TimeFormatTimeOnly},
 	}
 
 	location = time.UTC
 )
 
-func parseTime(s string) (time.Time, error) {
-	return parseTimeWith(s, location, timeFormats)
-}
+func parseTime(s string, args ...any) (d time.Time, e error) {
+	var (
+		location    *time.Location
+		timeFormats []TimeFormat
+	)
+	for _, arg := range args {
+		switch v := arg.(type) {
+		case time.Location:
+			location = &v
+		case TimeFormat:
+			timeFormats = append(timeFormats, v)
+		case *TimeFormat:
+			timeFormats = append(timeFormats, *v)
+		case []TimeFormat:
+			timeFormats = append(timeFormats, v...)
+		case []*TimeFormat:
+			for _, f := range v {
+				timeFormats = append(timeFormats, *f)
+			}
+		}
+	}
 
-func parseTimeWith(s string, location *time.Location, formats []timeFormat) (d time.Time, e error) {
-	for _, format := range formats {
-		if d, e = time.Parse(format.format, s); e == nil {
+	if location == nil {
+		location = time.Local
+	}
+	if len(timeFormats) == 0 {
+		timeFormats = defaultTimeFormats
+	}
+
+	for _, timeFormat := range timeFormats {
+		if d, e = time.Parse(timeFormat.Format, s); e == nil {
 
 			// Some time formats have a zone name, but no offset, so it gets
 			// put in that zone name (not the default one passed in to us), but
 			// without that zone's offset. So set the location manually.
-			if format.typ <= timeFormatNamedTimezone {
+			if timeFormat.Type <= TimeFormatNamedTimezone {
 				if location == nil {
 					location = time.Local
 				}
@@ -2662,11 +2686,20 @@ func init() {
 	durationRegExp = regexp.MustCompile(buf.String())
 }
 
-func parseDuration(s string) (time.Duration, error) {
-	return parseDurationWith(s, time.Now())
-}
+func parseDuration(s string, args ...any) (time.Duration, error) {
+	var (
+		t = time.Now()
+	)
 
-func parseDurationWith(s string, tm time.Time) (time.Duration, error) {
+	for _, arg := range args {
+		switch v := arg.(type) {
+		case time.Time:
+			t = v
+		case *time.Time:
+			t = *v
+		}
+	}
+
 	matches := durationRegExp.FindStringSubmatch(s)
 	if len(matches) == 0 {
 		return 0, nil
@@ -2693,7 +2726,7 @@ func parseDurationWith(s string, tm time.Time) (time.Duration, error) {
 		return 0, fmt.Errorf("parse duration `%s` failed", s)
 	}
 
-	duration := tm.AddDate(nums[0], nums[1], nums[2]).Add(
+	duration := t.AddDate(nums[0], nums[1], nums[2]).Add(
 		time.Duration(nums[3]) * time.Hour,
 	).Add(
 		time.Duration(nums[4]) * time.Minute,
@@ -2705,7 +2738,7 @@ func parseDurationWith(s string, tm time.Time) (time.Duration, error) {
 		time.Duration(nums[7]) * time.Microsecond,
 	).Add(
 		time.Duration(nums[8]) * time.Nanosecond,
-	).Sub(tm)
+	).Sub(t)
 
 	return duration, nil
 }
